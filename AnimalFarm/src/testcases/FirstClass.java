527459776a0cc467a4ef9cc673129690f6253809
Max Leuku Rudander <max.leuku@gmail.com>
2018-03-13 09:55:20 +0100

    First test class

---
+++ AnimalFarm/src/testcases/FirstClass.java
@@ -0,0 +1,12 @@
+package testcases;
+
+public class FirstClass {
+	public void firstMethod() {
+		System.out.println("Welcome to the farm.\rEnjoy your stay!");
+	}
+	public static void main (String [] args) {
+		FirstClass fc = new FirstClass();
+		fc.firstMethod();
+	}
+
+}
