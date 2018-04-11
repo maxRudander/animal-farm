c934ab2f4b4391e41bd2aee50710eedce0828c98
Malin <malinzed@gmail.com>
2018-04-11 21:33:05 +0200

    kommentarer inför sprint

--- AnimalFarm/src/farm/Pigsty.java
+++ AnimalFarm/src/farm/Pigsty.java
@@ -6,22 +6,48 @@ import javax.swing.ImageIcon;
 
 public class Pigsty extends Building {
 	
+	/**
+	 * Class that handles the Hen House Builing
+	 * 
+	 * @author Malin Zederfeldt
+	 */
+	
 	private Image pigsty;
+	
+	/**
+	 * Constructor that sends x, y coordinates to superclass and load images
+	 * @param x x coordinate
+	 * @param y y coordinate
+	 */
 	
 	public Pigsty(int x, int y) {
 		super(x,y);
 		loadImages();
 	}
 	
+	/**
+	 * Constructor only used when removing buildings, so coordinates needed
+	 */
+	
 	public Pigsty() {
 		super();
 		loadImages();
 	}
 	
+	/**
+	 * method loads image for building
+	 */
+	
 	public void loadImages() {
 		pigsty = new ImageIcon("images/pigsty.png").getImage();
 		
 	}
+	
+	/**
+	 * returns image of the building
+	 * 
+	 * @return image of building
+	 */
 	
 	public Image getImage() {
 		return pigsty;
--- AnimalFarm/src/farm/Stable.java
+++ AnimalFarm/src/farm/Stable.java
@@ -2,26 +2,53 @@ package farm;
 
 import java.awt.Image;
 
+/**
+ * Class that handles the Hen House Builing
+ * 
+ * @author Malin Zederfeldt
+ */
+
+
 import javax.swing.ImageIcon;
 
 public class Stable extends Building {
 	
 	private Image stable;
 	
+	/**
+	 * Constructor that sends x, y coordinates to superclass and load images
+	 * @param x x coordinate
+	 * @param y y coordinate
+	 */
+	
 	public Stable(int x, int y) {
 		super(x,y);
 		loadImages();
 	}
+	
+	/**
+	 * Constructor only used when removing buildings, so coordinates needed
+	 */
 	
 	public Stable() {
 		super();
 		loadImages();
 	}
 	
+	/**
+	 * method loads image for building
+	 */
+	
 	public void loadImages() {
 		stable = new ImageIcon("images/stable.png").getImage();
 		
 	}
+	
+	/**
+	 * returns image of the building
+	 * 
+	 * @return image of building
+	 */
 	
 	public Image getImage() {
 		return stable;
