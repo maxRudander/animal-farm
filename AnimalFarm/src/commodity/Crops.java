c290a32ae5ff960fd58a30a220a174195ae239da
mlindfors2 <mlindfors2@gmail.com>
2018-04-28 15:25:27 +0200

    Linked animals with buildings
    Buy a building before buying animals.

--- AnimalFarm/src/commodity/Crops.java
+++ AnimalFarm/src/commodity/Crops.java
@@ -5,18 +5,20 @@ import java.io.Serializable;
 
 import javax.swing.ImageIcon;
 
-import farm.Node;
+//import farm.Node;
 
 /**
  * author Elin O.
  */
-public class Crops implements Serializable{
+public class Crops implements Serializable {
 	private int x;
 	private int y;
-	
-	private Node[][] node;
+
+	// private Node[][] node;
+	private Boolean[][] node;
+
 	public Crops(int x, int y) {
-		
+
 		this.x = x;
 		this.y = y;
 	}
@@ -25,9 +27,11 @@ public class Crops implements Serializable{
 		x = 0;
 		y = 0;
 	}
-	public void setNode(Node[][] node) {
+
+	public void setNode(Boolean[][] node) {
 		this.node = node;
 	}
+
 	public void setX(int x) {
 		this.x = x;
 	}
@@ -47,20 +51,23 @@ public class Crops implements Serializable{
 	public ImageIcon getImage() {
 		return null;
 	}
-	public void setWalkableArea(int x1,int y1, int x2,int y2, boolean walkable) {
-		for (int x=x1;x<=x2;x++) {
-			setWalkable(x,y1,walkable);
-			setWalkable(x,y2,walkable);			
+
+	public void setWalkableArea(int x1, int y1, int x2, int y2, boolean walkable) {
+		for (int x = x1; x <= x2; x++) {
+			setWalkable(x, y1, walkable);
+			setWalkable(x, y2, walkable);
 		}
-		for (int y=y1;y<=y2;y++) {
-			setWalkable(x1,y,walkable);
-			setWalkable(x2,y,walkable);
+		for (int y = y1; y <= y2; y++) {
+			setWalkable(x1, y, walkable);
+			setWalkable(x2, y, walkable);
 		}
 	}
+
 	public void setWalkable(int x, int y, boolean walkable) {
-		node[x][y].setWalkable(walkable);
+		node[x][y] = walkable;
 	}
+
 	public boolean checkWalkable(int x, int y) {
-		return node[x][y].checkWalkable();
+		return node[x][y];
 	}
 }
